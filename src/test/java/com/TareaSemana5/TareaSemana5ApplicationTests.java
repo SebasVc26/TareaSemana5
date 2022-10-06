b7dfd3f2c2c152aa3849178657479a031d5c0a0b
SebasVc26 <sebas2000_vc@hotmail.com>
2022-10-06 00:34:21 -0600

    Avance tarea

---
+++ src/test/java/com/TareaSemana5/TareaSemana5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaSemana5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSemana5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
